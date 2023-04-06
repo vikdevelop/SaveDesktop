55aa50ba58a6de0b50f6575f7b80c128d1eb1687
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-04-06 10:43:22 +0200

    Update periodic_saving.py

--- src/periodic_saving.py
+++ src/periodic_saving.py
@@ -52,10 +52,6 @@ class PeriodicBackups:
                 print("Periodic backups are not set up.")
                 cont = "false"
             if cont == "true":
-                try:
-                    self.fpermissions = jp["save_flatpak_permissions"]
-                except:
-                    print("")
                 if self.pbackups == "Never":
                     print("Periodic backups are not set up.")
                     exit()
@@ -108,11 +104,7 @@ class PeriodicBackups:
             os.system('cp -R ~/.fonts ./')
             os.system('cp -R ~/.config/gtk-4.0 ./')
             os.system('cp -R ~/.config/gtk-3.0 ./')
-            try:
-                if self.fpermissions == 'True':
-                    os.system('cp -R ~/.local/share/flatpak/overrides ./')
-            except:
-                print("Option 'Save Flatpak custom permissions' not available.")
+            os.system('cp -R ~/.local/share/flatpak/overrides ./')
             # Save configs on individual desktop environments
             if self.environment == 'GNOME':
                 os.system("cp -R ~/.local/share/gnome-background-properties ./")
