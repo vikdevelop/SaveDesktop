4d9ce2220c030d1e4f599bcee93db114ead3c263
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-05-12 21:08:42 +0200

    Script for running without flatpak

---
+++ run_natively.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+git clone https://github.com/vikdevelop/SaveDesktop /tmp/SaveDesktop
+cd /tmp/SaveDesktop
+if [ "$1" == "" ]
+then 
+	python3 src/main_window.py
+elif [ "$1" == "--background" ]
+	then
+		echo "Periodic saving is currently only supported on Flatpak."
+fi
+
+cd
+rm -rf /tmp/SaveDesktop
