70b3fb83634459f07116b344d1f40da684a4f926
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-05-12 21:47:31 +0200

    Install app icons

--- run_natively.sh
+++ run_natively.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 #git clone https://github.com/vikdevelop/SaveDesktop /tmp/SaveDesktop
 cd /tmp/SaveDesktop
+install -D -t ~/.local/share/icons/hicolor/scalable/apps flatpak/icons/io.github.vikdevelop.SaveDesktop.svg
+install -D -t ~/.local/share/icons/hicolor/symbolic/apps flatpak/icons/io.github.vikdevelop.SaveDesktop-symbolic.svg
 if [ "$1" == "" ]
 then 
 	python3 src/main_window.py
