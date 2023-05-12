24836fbb1cac3d44cceae0edcd956b6d559c5c7b
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-05-12 21:50:10 +0200

    Update run_natively.sh

--- run_natively.sh
+++ run_natively.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 #git clone https://github.com/vikdevelop/SaveDesktop /tmp/SaveDesktop
 cd /tmp/SaveDesktop
+# Install app icons
 install -D -t ~/.local/share/icons/hicolor/scalable/apps flatpak/icons/io.github.vikdevelop.SaveDesktop.svg
 install -D -t ~/.local/share/icons/hicolor/symbolic/apps flatpak/icons/io.github.vikdevelop.SaveDesktop-symbolic.svg
 if [ "$1" == "" ]
@@ -8,7 +9,7 @@ then
 	python3 src/main_window.py
 elif [ "$1" == "--background" ]
 	then
-		echo "Periodic saving is currently only supported on Flatpak."
+		echo "Periodic saving is currently only supported in Flatpak."
 fi
 
 cd
