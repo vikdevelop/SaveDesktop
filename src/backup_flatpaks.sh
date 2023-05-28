13061620643b43df0c07a2faf55da542daf62bc3
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-05-28 15:36:55 +0200

    Update backup_flatpaks.sh

--- src/backup_flatpaks.sh
+++ src/backup_flatpaks.sh
@@ -1,1 +1,1 @@
-ls /var/lib/flatpak/app/ | awk '{print "flatpak install " $1 " -y"}' > ./installed_flatpaks.sh 
+ls /var/lib/flatpak/app/ | awk '{print "flatpak install --system " $1 " -y"}' > ./installed_flatpaks.sh 
