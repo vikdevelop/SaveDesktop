d84c473dca8124cb89f6cb5bebd028f8087cafe3
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-05-28 11:44:58 +0200

    Update backup_flatpaks.sh

--- src/backup_flatpaks.sh
+++ src/backup_flatpaks.sh
@@ -1,1 +1,1 @@
-flatpak-spawn --host flatpak list --app --columns=origin --columns=application | awk '{print "flatpak install " $1,$2 " -y"}' > ./installed_flatpaks.sh
+flatpak-spawn --host flatpak list --app --columns=origin --columns=application | awk '{print "flatpak-spawn --host flatpak install " $1,$2 " -y"}' > ./installed_flatpaks.sh
