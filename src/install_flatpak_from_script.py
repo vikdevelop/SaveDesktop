61734d5b707f2dab5e09ac1d16806a306d819366
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-06-02 19:32:55 +0200

    Update install_flatpak_from_script.py

--- src/install_flatpak_from_script.py
+++ src/install_flatpak_from_script.py
@@ -2,12 +2,14 @@
 import os
 from pathlib import Path
 
-flatpak = os.path.exists("/.flatpak-info")
+DATA_FLATPAK = f"{Path.home()}/.var/app/io.github.vikdevelop.SaveDesktop/data"
+DATA_NATIVE = f"{Path.home()}/.local/share/io.github.vikdevelop.SaveDesktop"
 
-DATA = f"{Path.home()}/.var/app/io.github.vikdevelop.SaveDesktop/data"
-
-if os.path.exists(f"{DATA}/installed_flatpaks.sh"):
-    os.system(f"sh {DATA}/installed_flatpaks.sh")
-    os.system(f"rm {DATA}/installed_flatpaks.sh")
+if os.path.exists(f"{DATA_FLATPAK}/installed_flatpaks.sh"):
+    os.system(f"sh {DATA_FLATPAK}/installed_flatpaks.sh")
+    os.system(f"rm {DATA_FLATPAK}/installed_flatpaks.sh")
+elif os.path.exists(f"{DATA_NATIVE}/installed_flatpaks.sh"):
+    os.system(f"sh {DATA_NATIVE}/installed_flatpaks.sh")
+    os.system(f"rm {DATA_NATIVE}/installed_flatpaks.sh")
 else:
     print("List with installed Flatpak apps is not exists.")
