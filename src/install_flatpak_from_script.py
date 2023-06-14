0fd1209a0ce73ff910099d9a31b4f7e79426949f
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-06-14 21:47:26 +0200

    Update install_flatpak_from_script.py

--- src/install_flatpak_from_script.py
+++ src/install_flatpak_from_script.py
@@ -5,6 +5,19 @@ from pathlib import Path
 DATA_FLATPAK = f"{Path.home()}/.var/app/io.github.vikdevelop.SaveDesktop/data"
 DATA_NATIVE = f"{Path.home()}/.local/share/io.github.vikdevelop.SaveDesktop"
 
+# Check Desktop Environment
+if os.getenv("XDG_CURRENT_DESKTOP") == 'GNOME':
+    environment = 'GNOME'
+elif os.getenv("XDG_CURRENT_DESKTOP") == 'ubuntu:GNOME':
+    environment = 'GNOME'
+elif os.getenv("XDG_CURRENT_DESKTOP") == 'zorin:GNOME':
+    environment = 'GNOME'
+
+# Activate gsettings property
+if environment == 'GNOME':
+    os.system("gsettings set org.gnome.shell disable-user-extensions false")
+
+# Install Flatpak apps from list
 if os.path.exists(f"{DATA_FLATPAK}/installed_flatpaks.sh"):
     os.system(f"sh {DATA_FLATPAK}/installed_flatpaks.sh")
     os.system(f"rm {DATA_FLATPAK}/installed_flatpaks.sh")
