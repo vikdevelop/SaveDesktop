3cdc04c2d2f8df19d29df346fc9c86fc3aaeb314
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-08-13 15:40:37 +0200

    Update network_sharing.py

--- src/network_sharing.py
+++ src/network_sharing.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
+from pathlib import Path
 import os
 import gi
 from gi.repository import Gio
 
-class NetworkSharing:
-    def __init__(self):
-        self.settings = Gio.Settings.new_with_path("io.github.vikdevelop.SaveDesktop", "/io/github/vikdevelop/SaveDesktop/")
-        
-        os.system("python -m http.server")
-        
-NetworkSharing()
+settings = Gio.Settings.new_with_path("io.github.vikdevelop.SaveDesktop", "/io/github/vikdevelop/SaveDesktop/")
+
+if settings["file-for-syncing"] == "":
+    print("Synchronization is not set up.")
+    exit()
+if settings["url-for-syncing"] == "":
+    print("Synchronization is not set up.")
+    exit()
+
+path = Path(settings["file-for-syncing"])
+folder = path.parent.absolute()
+
+file_name = os.path.basename(settings["file-for-syncing"])
+file = os.path.splitext(file_name)[0]
+
+os.chdir(folder)
+os.system(f"wget -c {settings['url-for-syncing']}/{file}.gz")
+print("Synchronization has been completed.")
