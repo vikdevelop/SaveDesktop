5118b1ff91609dbb389668fd507220208266d670
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-08-19 14:40:25 +0200

    Update network_sharing.py

--- src/network_sharing.py
+++ src/network_sharing.py
@@ -127,7 +127,7 @@ class Syncing:
         if not os.path.exists("{}/.config".format(Path.home())):
             os.system("mkdir ~/.config/")
         # Create Dconf directory
-        os.system("rm -rf ~/.config/dconf && mkdir ~/.config/dconf")
+        os.system("rm ~/.config/dconf/*")
         os.system(f"cp ./user {Path.home()}/.config/dconf/")
         os.system(f'cp -R ./icons {Path.home()}/.local/share/')
         os.system(f'cp -R ./.themes {Path.home()}/')
