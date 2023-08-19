0072d8bc17d1c6162a0422de6833e64968d8e0d5
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-08-19 10:39:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ elif [ "$1" == "--help" ]
 	then
    		echo -e '\033[1mArguments:\033[0m'
   		echo "--background | Start periodic saving"
-               echo "--sync | Sync desktop configuration with other computer"
+                echo "--sync | Sync desktop configuration with other computer"
   		echo "--start-server | Start HTTP server for synchronization DE config with other computers"
   		echo "--help | Show this message"
 fi
