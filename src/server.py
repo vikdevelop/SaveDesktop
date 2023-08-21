07a5dcb065eb18fc7bae0a049a14613deeaae00f
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-08-21 16:21:03 +0200

    Update server.py

--- src/server.py
+++ src/server.py
@@ -6,9 +6,6 @@ settings = Gio.Settings.new_with_path("io.github.vikdevelop.SaveDesktop", "/io/g
 if settings["file-for-syncing"] == "":
     print("Synchronization is not set up.")
     exit()
-elif settings["url-for-syncing"] == "":
-    print("Synchronization is not set up.")
-    exit()
 else:
     path = Path(settings["file-for-syncing"])
     folder = path.parent.absolute()
