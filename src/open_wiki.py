b8a52a513906207d1ef1b00a92462fef3d9a6234
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-08-24 19:15:11 +0200

    Create open_wiki.py

---
+++ src/open_wiki.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import locale
+import json
+import os
+# Load system language
+p_lang = locale.getlocale()[0]
+if p_lang == 'pt_BR':
+    r_lang = 'pt_BR'
+elif p_lang == 'nb_NO':
+    r_lang = 'nb_NO'
+elif 'zh' in p_lang:
+    r_lang = 'zh_Hans'
+else:
+    r_lang = p_lang[:-3]
+    
+new_lang = p_lang.replace("_", "-")
+
+if os.path.exists(f"/app/translations/wiki/{r_lang}.xml"):
+    pb_wiki = f"https://github.com/vikdevelop/SaveDesktop/wiki/Periodic-saving-{new_lang}" 
+    flatpak_wiki = f"https://github.com/vikdevelop/SaveDesktop/wiki/Save-installed-Flatpak-apps-and-install-it-from-list-{new_lang}"
+    sync_wiki = f"https://github.com/vikdevelop/SaveDesktop/wiki/Synchronization-between-computers-in-the-network-{new_lang}"
+else:
+    pb_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Periodic-saving"
+    flatpak_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Save-installed-Flatpak-apps-and-install-it-from-list"
+    sync_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Synchronization-between-computers-in-the-network"
