45b14e9b39cb7fd32c305afc3ac53359a4f85077
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-09-18 18:09:41 +0200

    Update server.py

--- src/server.py
+++ src/server.py
@@ -3,9 +3,12 @@ import sys
 from pathlib import Path
 from gi.repository import Gio
 settings = Gio.Settings.new_with_path("io.github.vikdevelop.SaveDesktop", "/io/github/vikdevelop/SaveDesktop/")
+
 if settings["file-for-syncing"] == "":
     print("Synchronization is not set up.")
     exit()
+elif settings["periodic-import"] == "Never2":
+    print("Synchronization is not set up.")
 else:
     path = Path(settings["file-for-syncing"])
     folder = path.parent.absolute()
