b76af167f35b8135f31a7afd0c43239ea64bcb5e
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-09-22 21:16:55 +0200

    Update open_wiki.py

--- src/open_wiki.py
+++ src/open_wiki.py
@@ -2,17 +2,8 @@
 import locale
 import json
 import os
+from localization import *
 import subprocess
-# Load system language
-p_lang = locale.getlocale()[0]
-if p_lang == 'pt_BR':
-    r_lang = 'pt_BR'
-elif p_lang == 'nb_NO':
-    r_lang = 'nb_NO'
-elif 'zh' in p_lang:
-    r_lang = 'zh_Hans'
-else:
-    r_lang = p_lang[:-3]
     
 new_lang = p_lang.replace("_", "-")
 
