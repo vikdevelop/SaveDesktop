e8673a95cacc4c1e9811c0be4fc979a8a0ba8539
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-09-30 17:39:58 +0200

    Update network_sharing.py

--- src/network_sharing.py
+++ src/network_sharing.py
@@ -143,8 +143,8 @@ class Syncing:
         # Applying configuration for GNOME-based environments
         if not os.path.exists("{}/.config".format(Path.home())):
             os.system("mkdir ~/.config/")
-        os.system(f"cp ./user {Path.home()}/.config/dconf/")
-        os.system(f"cp ./user {DATA}/")
+        os.system(f"cp -r ./user {Path.home()}/.config/dconf/")
+        os.system(f"cp -r ./user {DATA}/")
         os.system(f'cp -R ./icons {Path.home()}/.local/share/')
         os.system(f'cp -R ./.themes {Path.home()}/')
         os.system(f'cp -R ./.icons {Path.home()}/')
