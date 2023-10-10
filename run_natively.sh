f79c303766253f684e2834393434510bc060155d
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-10-10 21:16:47 +0200

    Update run_natively.sh

--- run_natively.sh
+++ run_natively.sh
@@ -5,7 +5,16 @@ then
 	python3 src/main_window.py
 elif [ "$1" == "--background" ]
 	then
-		echo "Periodic saving is currently only supported in Flatpak."
+		python3 src/periodic_saving.py
+elif [ "$1" == "--sync" ]
+	then
+		python3 src/network_sharing.py
+elif [ "$1" == "--start-server" ]
+	then
+		python3 src/server.py
+elif [ "$1" == "--help" ]
+	then
+   		echo -e '\033[1mArguments:\033[0m \n None | Run SaveDesktop app (GUI) \n --background | Start periodic saving \n --sync | Sync desktop configuration with other computer \n --start-server | Start HTTP server for synchronization DE config with other computers \n --help | Show this message'
 fi
 
 cd
