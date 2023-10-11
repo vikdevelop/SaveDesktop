14fbc55cab3b69e34de2fbf542f8cb9213aab2be
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-10-11 19:30:40 +0200

    Update network_sharing.py

--- src/network_sharing.py
+++ src/network_sharing.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from datetime import datetime
 from datetime import date
-from localization import _, CACHE, DATA, IPAddr, system_dir
+from localization import _, CACHE, DATA, IPAddr, system_dir, flatpak
 import os
 import locale
 import json
