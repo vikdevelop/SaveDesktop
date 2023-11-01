4912f3e9558b9166e729f5b0df434d6fd39fc68c
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-11-01 13:13:32 +0530

    changed for Snap to run

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
-if [ "$1" == "" ]
+
+if [ "$SNAP" == "" ]
+then 
+	if [ "$1" == "" ]
 then 
 	python3 /app/main_window.py
 elif [ "$1" == "--background" ]
@@ -15,3 +18,21 @@ elif [ "$1" == "--help" ]
 	then
    		echo -e '\033[1mArguments:\033[0m \n None | Run SaveDesktop app (GUI) \n --background | Start periodic saving \n --sync | Sync desktop configuration with other computer \n --start-server | Start HTTP server for synchronization DE config with other computers \n --help | Show this message'
 fi
+else
+	if [ "$1" == "" ]
+	then 
+		python3 $SNAP/usr/main_window.py
+	elif [ "$1" == "--background" ]
+		then
+			python3 $SNAP/usr/periodic_saving.py
+	elif [ "$1" == "--sync" ]
+		then
+			python3 $SNAP/usr/network_sharing.py
+	elif [ "$1" == "--start-server" ]
+		then
+			python3 $SNAP/usr/server.py
+	elif [ "$1" == "--help" ]
+		then
+		echo -e '\033[1mArguments:\033[0m \n None | Run SaveDesktop app (GUI) \n --background | Start periodic saving \n --sync | Sync desktop configuration with other computer \n --start-server | Start HTTP server for synchronization DE config with other computers \n --help | Show this message'
+fi
+fi 
