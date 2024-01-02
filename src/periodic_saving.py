01c7947b0528dfd56c8a20ef42d8eb87d18ac8f6
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-01-02 12:03:19 +0100

    Update periodic_saving.py

--- src/periodic_saving.py
+++ src/periodic_saving.py
@@ -5,7 +5,7 @@ import json
 import os
 import gi
 from gi.repository import GLib, Gio
-from localization import _, CACHE, home
+from localization import _, CACHE, home, system_dir
 
 # get current datetime
 dt = datetime.now()
@@ -87,74 +87,18 @@ class PeriodicBackups:
                 os.system(f"mkdir {home}/Downloads")
                 os.system(f"xdg-user-dirs-update --set DOWNLOAD {home}/Downloads")
                 os.makedirs(f"{download_dir}/SaveDesktop/archives")
-        if not os.path.exists(f"{CACHE}/periodic-saving"):
-        	os.mkdir(f"{CACHE}/periodic-saving")
-        os.chdir(f"{CACHE}/periodic-saving")
-        os.system("dconf dump / > ./dconf-settings.ini")
-        #os.system('cp -R {home}/.config/dconf/user ./')
-        os.system(f'cp -R {home}/.config/gtk-4.0 ./')
-        os.system(f'cp -R {home}/.config/gtk-3.0 ./')
-        if self.settings["save-backgrounds"] == True:
-            os.system(f'cp -R {home}/.local/share/backgrounds ./')
-        if self.settings["save-icons"] == True:
-            os.system(f'cp -R {home}/.local/share/icons ./')
-            os.system(f'cp -R {home}/.icons ./')
-        if self.settings["save-themes"] == True:
-            os.system(f'cp -R {home}/.themes ./')
-        if self.settings["save-fonts"] == True:
-            os.system(f'cp -R {home}/.fonts ./')
-        if self.settings["save-installed-flatpaks"] == True:
-            os.system('sh /app/backup_flatpaks.sh')
-        # Save configs on individual desktop environments
-        if self.environment == 'GNOME':
-            os.system(f"cp -R {home}/.local/share/gnome-background-properties ./")
-            os.system(f"cp -R {home}/.local/share/gnome-shell ./")
-            os.system(f"cp -R {home}/.local/share/nautilus-python ./")
-            os.system(f"cp -R {home}/.config/gnome-control-center ./")
-        elif self.environment == 'Pantheon':
-            os.system(f"cp -R {home}/.config/plank ./")
-            os.system(f"cp -R {home}/.config/marlin ./")
-        elif self.environment == 'Cinnamon':
-            os.system(f"cp -R {home}/.config/nemo ./")
-            os.system(f"cp -R {home}/.local/share/cinnamon ./")
-            os.system(f"cp -R {home}/.cinnamon ./")
-        elif self.environment == 'Budgie':
-            os.system(f"cp -R {home}/.config/budgie-desktop ./")
-            os.system(f"cp -R {home}/.config/budgie-extras ./")
-            os.system(f"cp -R {home}/.config/nemo ./")
-        elif self.environment == 'COSMIC':
-            os.system(f"cp -R {home}/.config/pop-shell ./")
-            os.system(f"cp -R {home}/.local/share/gnome-shell ./")
-        elif self.environment == 'Xfce':
-            os.system(f"cp -R {home}/.config/xfce4 ./")
-            os.system(f"cp -R {home}/.config/Thunar ./")
-            os.system(f"cp -R {home}/.xfce4 ./")
-        elif self.environment == 'MATE':
-            os.system(f"cp -R {home}/.config/caja ./")
-        elif self.environment == 'KDE Plasma':
-            os.system("mkdir xdg-config && mkdir xdg-data")
-            os.system(f"cp -R {home}/.config/[k]* ./xdg-config/")
-            os.system(f"cp {home}/.config/gtkrc ./xdg-config/")
-            os.system(f"cp {home}/.config/dolphinrc ./xdg-config/")
-            os.system(f"cp {home}/.config/gwenviewrc ./xdg-config/")
-            os.system(f"cp {home}/.config/plasmashellrc ./xdg-config/")
-            os.system(f"cp {home}/.config/spectaclerc ./xdg-config/")
-            os.system(f"cp {home}/.config/plasmarc ./xdg-config/")
-            os.system(f"cp {home}/.config/plasma-org.kde.plasma.desktop-appletsrc ./xdg-config/")
-            os.system(f"cp -R {home}/.local/share/[k]* ./xdg-data/")
-            os.system(f"cp -R {home}/.local/share/dolphin ./xdg-data/")
-            os.system(f"cp -R {home}/.local/share/sddm ./xdg-data/")
-            if self.settings["save-backgrounds"] == True:
-                os.system(f"cp -R {home}/.local/share/wallpapers ./xdg-data/")
-            os.system(f"cp -R {home}/.local/share/plasma-systemmonitor ./xdg-data/")
+        if not os.path.exists(f"{CACHE}/periodic_saving"):
+            os.mkdir(f"{CACHE}/periodic_saving")
         if " " in self.settings["filename-format"]:
             old_filename = f'{self.settings["filename-format"]}'
             filename = old_filename.replace(" ", "_")
         else:
             filename = self.settings["filename-format"]
-        # Create Tar.gz archive
-        os.system(f"tar --gzip -cf {filename}.sd.tar.gz ./")
-        os.system("mv ./*.tar.gz {}/".format(self.pbfolder))
+        with open(f"{CACHE}/.periodicfile.json", "w") as p:
+            p.write('{\n "recent_file": "%s/%s.sd.tar.gz"\n}' % (self.pbfolder, filename))
+        os.chdir(f"{CACHE}/periodic_saving")
+        os.system(f"python3 {system_dir}/config.py --save")
+        os.system(f"rm {CACHE}/.periodicfile.json")
         self.config_saved()
       
     # Message about saved config
