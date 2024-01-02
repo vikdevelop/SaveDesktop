8e96f651f655aac0224f2845a775f2690f445d7b
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-01-02 16:59:11 +0100

    Update install_flatpak_from_script.py

--- src/install_flatpak_from_script.py
+++ src/install_flatpak_from_script.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 
 DATA_FLATPAK = f"{Path.home()}/.var/app/io.github.vikdevelop.SaveDesktop/data"
+CACHE_FLATPAK = f"{Path.home()}/.var/app/io.github.vikdevelop.SaveDesktop/cache/tmp"
 
 # Check Desktop Environment
 if os.getenv("XDG_CURRENT_DESKTOP") == 'GNOME':
@@ -18,9 +19,17 @@ else:
 if environment == 'GNOME':
     os.system("gsettings set org.gnome.shell disable-user-extensions false")
 
+if os.path.exists(f"{DATA_FLATPAK}/app"):
+    with open(f"{CACHE_FLATPAK}/copying_flatpak_data", "w") as f:
+        f.write("copying flatpak data ...")
+    os.system(f"cp -R {CACHE_FLATPAK}/import_config/app {DATA_FLATPAK}/")
+    os.system(f"cp -R {DATA_FLATPAK}/app ~/.var/")
+    os.system(f"rm {CACHE_FLATPAK}/*")
+
 # Install Flatpak apps from list
 if os.path.exists(f"{DATA_FLATPAK}/installed_flatpaks.sh"):
     os.system(f"sh {DATA_FLATPAK}/installed_flatpaks.sh && sh {DATA_FLATPAK}/installed_user_flatpaks.sh")
     os.system(f"rm {DATA_FLATPAK}/*.sh")
 else:
     print("List with installed Flatpak apps is not exists.")
+    
