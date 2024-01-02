a820856537f73e34f8c3420059cc6fd9ef89d838
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-01-02 16:03:25 +0100

    add support for saving flatpaks in the ~/.local/share/flatpak/app folder

--- src/backup_flatpaks.sh
+++ src/backup_flatpaks.sh
@@ -1,1 +1,2 @@
-ls /var/lib/flatpak/app/ | awk '{print "flatpak install --system " $1 " -y"}' > ./installed_flatpaks.sh 
+ls /var/lib/flatpak/app/ | awk '{print "flatpak install --system " $1 " -y"}' > ./installed_flatpaks.sh
+ls ~/.local/share/flatpak/app | awk '{print "flatpak install --user " $1 " -y"}' > ./installed_user_flatpaks.sh
