d967f6285f029034df121b84df2b550c24f49f2e
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-01-02 11:36:13 +0100

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import glob
 from pathlib import Path
 from gi.repository import Gio
+from localization import _, DATA
 settings = Gio.Settings.new_with_path("io.github.vikdevelop.SaveDesktop", "/io/github/vikdevelop/SaveDesktop/")
 
 if settings["file-for-syncing"] == "":
@@ -10,9 +12,17 @@ if settings["file-for-syncing"] == "":
 elif settings["periodic-import"] == "Never2":
     print("Synchronization is not set up.")
 else:
-    path = Path(settings["file-for-syncing"])
-    folder = path.parent.absolute()
+    if not os.path.exists(f"{DATA}/synchronization"):
+        os.mkdir(f"{DATA}/synchronization")
+    if not glob.glob(f"{DATA}/*.sd.tar.gz"):
+        os.system(f"cp {settings['file-for-syncing']} {DATA}/synchronization/")
+    if not os.path.exists(f"{DATA}/synchronization/file-settings.json"):
+        file_name = os.path.basename(settings["file-for-syncing"])
+        with open(f"{DATA}/synchronization/file-settings.json", "w") as f:
+            f.write('{\n "file-name": "%s.gz",\n "periodic-import": "%s"\n}' % (file_name, settings["periodic-import"]))
+    
+    path = Path(f'{DATA}/synchronization')
 
-    sys.path.append(folder)
-    os.chdir(folder)
+    sys.path.append(path)
+    os.chdir(path)
     os.system("python3 -m http.server")
