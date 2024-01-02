35b97e67862baeca2f44387d60ab1467ab34d16b
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-01-02 16:14:37 +0100

    Update main_window.py

--- src/main_window.py
+++ src/main_window.py
@@ -1084,7 +1084,7 @@ class MyApp(Adw.Application):
             dialog.set_version(version)
             dialog.set_application_icon(icon)
         dialog.set_release_notes("<ul>\
-                   <li>Added support for saving user data of installed Flatpak apps. You can select if you want to save a list of installed Flatpak apps only or also save the data of your installed Flatpak apps.</li>\
+                   <li>Added support for saving user data from installed Flatpak apps. You can select if you want to save a list of installed Flatpak apps only or also save the data of your installed Flatpak apps.</li>\
                    <li>Added support for saving flatpaks installed in the home folder (~/.local/share/flatpak/app)</li>\
                    <li>Improved saving and importing configuration</li>\
                    <li>Improved selecting the sync file; you don't need to allow permission in the Flatseal app and check if the folder for the sync file is the same as the periodic saving folder</li>\
