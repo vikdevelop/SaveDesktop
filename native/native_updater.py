492d27f3f9d2902bb483f633ae857ff12eb31d9a
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-01-14 18:55:22 +0100

    Update native_updater.py

--- native/native_updater.py
+++ native/native_updater.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Get info about version
 response = requests.get("https://api.github.com/repos/vikdevelop/SaveDesktop/releases/latest")
-github_version = response.json()["name"]
+github_version = response.json()["tag_name"]
 
 sys.path.append(f"{Path.home()}/.local/share/savedesktop")
 os.chdir(f"{Path.home()}/.local/share/savedesktop")
@@ -15,5 +15,5 @@ os.chdir(f"{Path.home()}/.local/bin")
 d_ver = version.replace("-native", "")
 
 if not d_ver >= f"{github_version}":
-    os.system("wget -qO /tmp/savedesktop-native-installer.py https://raw.githubusercontent.com/vikdevelop/SaveDesktop/main/native/native_installer.py && python3 /tmp/savedesktop-native-installer.py --install")
     os.system("wget -qO /tmp/savedesktop-native-installer.py https://raw.githubusercontent.com/vikdevelop/SaveDesktop/main/native/native_installer.py && python3 /tmp/savedesktop-native-installer.py --remove")
+    os.system("wget -qO /tmp/savedesktop-native-installer.py https://raw.githubusercontent.com/vikdevelop/SaveDesktop/main/native/native_installer.py && python3 /tmp/savedesktop-native-installer.py --install")
