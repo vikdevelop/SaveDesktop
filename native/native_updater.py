9fd8bf5b2306390061d956fb3516c4ecb6347ad7
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-01-14 19:15:43 +0100

    improve update flow in the native version

--- native/native_updater.py
+++ native/native_updater.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import os
+import subprocess
 from pathlib import Path
 
 # Get info about version
@@ -12,8 +13,9 @@ os.chdir(f"{Path.home()}/.local/share/savedesktop")
 from src.localization import *
 os.chdir(f"{Path.home()}/.local/bin")
 
-d_ver = version.replace("-native", "")
+d_ver = subprocess.getoutput("cat ~/.local/share/io.github.vikdevelop.SaveDesktop/version.txt")
 
-if not d_ver >= f"{github_version}":
+if not d_ver == f"{github_version}":
     os.system("wget -qO /tmp/savedesktop-native-installer.py https://raw.githubusercontent.com/vikdevelop/SaveDesktop/main/native/native_installer.py && python3 /tmp/savedesktop-native-installer.py --remove")
     os.system("wget -qO /tmp/savedesktop-native-installer.py https://raw.githubusercontent.com/vikdevelop/SaveDesktop/main/native/native_installer.py && python3 /tmp/savedesktop-native-installer.py --install")
+    os.system(f'echo "{github_version}" > ~/.local/share/io.github.vikdevelop.SaveDesktop/version.txt')
