50d664f2bd459e3365f9b4665fad5e349b4d6cd4
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-06-27 20:26:12 +0200

    Update open_wiki.py

--- src/open_wiki.py
+++ src/open_wiki.py
@@ -8,10 +8,10 @@ import subprocess
 new_lang = p_lang.replace("_", "-")
 
 if "The requested URL returned error: 404" in subprocess.getoutput(f"curl --head --fail https://raw.githubusercontent.com/vikdevelop/SaveDesktop/main/translations/wiki/{r_lang}.xml"):
-    pb_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/periodic-saving/"
-    flatpak_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/flatpak-apps-installation/"
-    sync_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/synchronization/"
-    enc_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/archive-encryption/"
+    pb_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/periodic-saving/en"
+    flatpak_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/flatpak-apps-installation/en"
+    sync_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/synchronization/en"
+    enc_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/archive-encryption/en"
     lang_list = False
 elif r_lang == "en":
     pb_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/periodic-saving/en"
