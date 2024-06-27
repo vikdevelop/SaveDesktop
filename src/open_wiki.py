1de9d685131098327ccfb42ae143ac74054bbad5
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-06-27 20:11:20 +0200

    set new wiki link

--- src/open_wiki.py
+++ src/open_wiki.py
@@ -8,20 +8,20 @@ import subprocess
 new_lang = p_lang.replace("_", "-")
 
 if "The requested URL returned error: 404" in subprocess.getoutput(f"curl --head --fail https://raw.githubusercontent.com/vikdevelop/SaveDesktop/main/translations/wiki/{r_lang}.xml"):
-    pb_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Periodic-saving"
-    flatpak_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Save-installed-Flatpak-apps-and-install-it-from-list"
-    sync_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Synchronization-between-computers-in-the-network"
-    enc_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Archive-encryption"
+    pb_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/periodic-saving/"
+    flatpak_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/flatpak-apps-installation/"
+    sync_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/synchronization/"
+    enc_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/archive-encryption/"
     lang_list = False
 elif r_lang == "en":
-    pb_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Periodic-saving"
-    flatpak_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Save-installed-Flatpak-apps-and-install-it-from-list"
-    sync_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Synchronization-between-computers-in-the-network"
-    enc_wiki = "https://github.com/vikdevelop/SaveDesktop/wiki/Archive-encryption"
+    pb_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/periodic-saving/"
+    flatpak_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/flatpak-apps-installation/"
+    sync_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/synchronization/"
+    enc_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/archive-encryption/"
     lang_list = False
 else:
-    pb_wiki = f"https://github.com/vikdevelop/SaveDesktop/wiki/Periodic-saving-{r_lang}" 
-    flatpak_wiki = f"https://github.com/vikdevelop/SaveDesktop/wiki/Save-installed-Flatpak-apps-and-install-it-from-list-{r_lang}"
-    sync_wiki = f"https://github.com/vikdevelop/SaveDesktop/wiki/Synchronization-between-computers-in-the-network-{r_lang}"
-    enc_wiki = f"https://github.com/vikdevelop/SaveDesktop/wiki/Archive-encryption-{r_lang}"
+    pb_wiki = f"https://vikdevelop.github.io/SaveDesktop/wiki/periodic-saving/{r_lang}"
+    flatpak_wiki = f"https://vikdevelop.github.io/SaveDesktop/wiki/flatpak-apps-installation/{r_lang}"
+    sync_wiki = f"https://vikdevelop.github.io/SaveDesktop/wiki/synchronization/{r_lang}"
+    enc_wiki = f"https://vikdevelop.github.io/SaveDesktop/wiki/archive-encryption/{r_lang}"
     lang_list = True
