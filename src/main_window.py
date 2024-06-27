9313168f608feb264f4d607fc58cd8aeb892f534
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-06-27 12:03:50 +0200

    Update main_window.py

--- src/main_window.py
+++ src/main_window.py
@@ -1730,7 +1730,7 @@ class MyApp(Adw.Application):
         if not r_lang == "en":
             dialog.set_translator_credits(_["translator_credits"])
         print(lang_list)
-        if not lang_list:
+        if not lang_list == False:
             dialog.add_link("Translate SaveDesktop Github Wiki", "https://hosted.weblate.org/projects/vikdevelop/savedesktop-github-wiki/")
         dialog.set_license_type(Gtk.License(Gtk.License.GPL_3_0))
         dialog.set_website("https://github.com/vikdevelop/SaveDesktop")
