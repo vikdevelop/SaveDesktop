b07e747c30003d3a58550f3e45af26b13ce29c9b
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-08-21 11:00:03 +0200

    fix bug with importing the ZIP archives in the CLI

--- run.sh
+++ run.sh
@@ -19,6 +19,15 @@ if [ "$SNAP" == "" ]; then
         mkdir $CACHE/import_config
         rm -rf $CACHE/import_config/*
         cd $CACHE/import_config
+        if [[ "$2" == *.sd.tar.gz ]]; then
+                echo "decompressing Tar archive"
+        elif [[ "$2" == *.sd.zip ]]; then
+                echo "decrypting zip archive"
+                unzip -o "$2"
+        else
+                echo "You are selected an unsupported file type. The supported files are: *.sd.tar.gz and *.sd.zip"
+                exit
+        fi
         python3 /app/config.py --import_
     elif [ "$1" == "--help" ]; then
         echo -e '\033[1mArguments:\033[0m \n None | Run SaveDesktop app (GUI) \n --background | Start periodic saving \n --sync | Sync desktop configuration with other computer \n --start-server | Start HTTP server for synchronization DE config with other computers \n --import-config /path/to/filename.sd.tar.gz | Import configuration of DE \n --save-now | Save configuration of DE using parameters from UI\n --help | Show this message'
@@ -39,6 +48,15 @@ else
         mkdir $CACHE_SNAP/import_config
         rm -rf $CACHE_SNAP/import_config/*
         cd $CACHE_SNAP/import_config
+        if [[ "$2" == *.sd.tar.gz ]]; then
+                echo "decompressing Tar archive"
+        elif [[ "$2" == *.sd.zip ]]; then
+                echo "decrypting zip archive"
+                unzip -o "$2"
+        else
+                echo "You are selected an unsupported file type. The supported files are: *.sd.tar.gz and *.sd.zip"
+                exit
+        fi
         python3 $SNAP/usr/config.py --import_
     elif [ "$1" == "--help" ]; then
         echo -e '\033[1mArguments:\033[0m \n None | Run SaveDesktop app (GUI) \n --background | Start periodic saving \n --sync | Sync desktop configuration with other computer \n --start-server | Start HTTP server for synchronization DE config with other computers \n --import-config /path/to/filename.sd.tar.gz | Import configuration of DE \n --save-now | Save configuration of DE using parameters from UI\n --help | Show this message'
