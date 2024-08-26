372a48609385d6f4a4609e2acca7c824c2eaa2bc
vikdevelop <vikdevelop@proton.me>
2024-08-26 17:55:45 +0200

    move the FlatpakAppsDialog to another script

--- src/main_window.py
+++ src/main_window.py
@@ -1,147 +1,15 @@
 #!/usr/bin/python3
-import os, socket, glob, sys, shutil, re, zipfile, random, string
+import os, socket, glob, sys, shutil, re, zipfile, random, string, gi
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
 from urllib.request import urlopen
 from datetime import date
 from pathlib import Path
 from threading import Thread
-import gi
-gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw, Gio, GLib
-from localization import _, home
+from localization import _, home, download_dir
 from open_wiki import *
 from shortcuts_window import *
-
-# Get user download dir
-download_dir = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOWNLOAD)
-
-# for SNAP: create the cache directory and import dbus module
-if snap:
-    import dbus
-    os.makedirs(f"{CACHE}", exist_ok=True)
-
-# load GSettings database for viewing and saving user settings of the app
-settings = Gio.Settings.new_with_path("io.github.vikdevelop.SaveDesktop", "/io/github/vikdevelop/SaveDesktop/")
-
-# Shortcuts window
-@Gtk.Template(string=SHORTCUTS_WINDOW) # from shortcuts_window.py
-class ShortcutsWindow(Gtk.ShortcutsWindow):
-    __gtype_name__ = 'ShortcutsWindow'
-    
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-# Row for showing available apps
-class FolderSwitchRow(Gtk.ListBoxRow):
-    def __init__(self, folder_name):
-        super().__init__()
-        self.folder_name = folder_name
-        
-        # switch for all items
-        self.switch = Gtk.Switch()
-        self.switch.set_halign(Gtk.Align.END)
-        self.switch.set_valign(Gtk.Align.CENTER)
-        self.switch.connect("state-set", self.on_switch_activated)
-        if settings["disabled-flatpak-apps-data"] == []:
-            self.switch.set_active(True)
-        
-        # row for all items
-        self.approw = Adw.ActionRow.new()
-        self.approw.set_title(folder_name)
-        self.approw.add_suffix(self.switch)
-        self.approw.set_title_lines(4)
-        self.approw.set_activatable_widget(self.switch)
-        self.approw.set_hexpand(True)
-        
-        # box for self.approw
-        self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-        self.box.append(self.approw)
-        
-        self.set_child(self.box)
-        
-        # set switch states from the Gsettings database
-        switch_state = folder_name not in settings.get_strv("disabled-flatpak-apps-data")
-        self.switch.set_active(switch_state)
-    
-    # save switch state
-    def on_switch_activated(self, switch, state):
-        disabled_flatpaks = settings.get_strv("disabled-flatpak-apps-data")
-        if not state:
-            if self.folder_name not in disabled_flatpaks:
-                disabled_flatpaks.append(self.folder_name)
-        else:
-            if self.folder_name in disabled_flatpaks:
-                disabled_flatpaks.remove(self.folder_name)
-        settings.set_strv("disabled-flatpak-apps-data", disabled_flatpaks)
-
-# dialog for showing installed Flatpak apps
-class FlatpakAppsDialog(Adw.MessageDialog):
-    def __init__(self):
-        super().__init__(transient_for=app.get_active_window())
-        self.set_heading(_["flatpaks_data_tittle"])
-        self.set_default_size(300, 400)
-        
-        # primary Gtk.Box for this dialog
-        self.dialogBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
-        self.set_extra_child(self.dialogBox)
-        
-        self.old_disabled_flatpaks = settings["disabled-flatpak-apps-data"]
-        
-        # widget for scrolling items list
-        scrolled_window = Gtk.ScrolledWindow()
-        scrolled_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-        scrolled_window.set_min_content_width(300)
-        scrolled_window.set_min_content_height(380)
-        self.dialogBox.append(scrolled_window)
-        
-        # listbox for showing items
-        self.flowbox = Gtk.ListBox.new()
-        self.flowbox.set_selection_mode(mode=Gtk.SelectionMode.NONE)
-        self.flowbox.add_css_class(css_class='boxed-list')
-        
-        # set self.flowbox as child for Gtk.ScrolledWindow widget
-        scrolled_window.set_child(self.flowbox)
-        
-        # add buttons to the dialog
-        self.add_response('cancel', _["cancel"])
-        self.add_response('ok', _["apply"])
-        self.set_response_appearance('ok', Adw.ResponseAppearance.SUGGESTED)
-        self.connect('response', self.apply_settings)
-        
-        # if there are problems loading a folder, an error message is displayed
-        try:
-            self.load_folders()
-            self.set_initial_switch_state()
-        except Exception as e:
-            self.set_body(f"Error: {e}")
-    
-    # load items from ~/.var/app directory
-    def load_folders(self):
-        folder_path = f"{home}/.var/app"
-        try:
-            folder = Gio.File.new_for_path(folder_path)
-            files = folder.enumerate_children("standard::name", Gio.FileQueryInfoFlags.NONE, None)
-            while True:
-                file_info = files.next_file(None)
-                if file_info is None:
-                    break
-                folder_name = file_info.get_name()
-                folder_row = FolderSwitchRow(folder_name)
-                self.flowbox.append(folder_row)
-        except Exception as e:
-            print(f"Error loading folders: {e}")
-       
-    # set default switch state
-    def set_initial_switch_state(self):
-        disabled_flatpaks = settings.get_strv("disabled-flatpak-apps-data")
-        for child in self.flowbox.get_row_at_index(0):
-            if isinstance(child, FolderSwitchRow):
-                child.switch.set_active(child.folder_name not in disabled_flatpaks)
-    
-    # if user clicks on the cancel button, the settings will not saved
-    def apply_settings(self, w, response):
-        if response == 'cancel':
-            settings["disabled-flatpak-apps-data"] = self.old_disabled_flatpaks
     
 # Application window
 class MainWindow(Adw.ApplicationWindow):
@@ -1045,8 +913,10 @@ class MainWindow(Adw.ApplicationWindow):
 
         # show dialog for managing Flatpak applications data
         def manage_data_list(w):
+            from flatpak_apps_dialog import FolderSwitchRow, FlatpakAppsDialog
             self.itemsDialog.close()
             self.appd = FlatpakAppsDialog()
+            self.appd.set_transient_for(app.get_active_window())
             self.appd.show()
 
         # show button after clicking on the switch "User data of Flatpak apps"
@@ -1403,6 +1273,7 @@ class MainWindow(Adw.ApplicationWindow):
     def please_wait_save(self):
         # Stop saving configuration
         def cancel_save(w):
+            # VYRESIT PROBLEM S UKONCENIM UKLADANI!!!!
             cancel_thread_save = Thread(target=self.cancel_saving_or_importing)
             cancel_thread_save.start()
             self.toolbarview.set_content(self.headapp)
@@ -1593,6 +1464,7 @@ class MainWindow(Adw.ApplicationWindow):
     def please_wait_import(self):
         # Stop importing configuration
         def cancel_import(w):
+            # VYRESIT PROBLEM S UKONCENIM IMPORTU!!!!
             cancel_thread_import = Thread(target=self.cancel_saving_or_importing)
             cancel_thread_import.start()
             self.toolbarview.set_content(self.headapp)
@@ -1748,7 +1620,7 @@ class MainWindow(Adw.ApplicationWindow):
             print("Flatpak data exists.")
         else:
             os.system(f"rm -rf {CACHE}/* {CACHE}/.*")
-        
+
 class MyApp(Adw.Application):
     def __init__(self, **kwargs):
         super().__init__(**kwargs, flags=Gio.ApplicationFlags.FLAGS_NONE, 
