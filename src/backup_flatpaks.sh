a60ca7fe5bf9acbb572382a35653f980c08d4a80
vikdevelop <vikdevelop@proton.me>
2024-08-26 18:11:19 +0200

    Delete src/backup_flatpaks.sh

--- src/backup_flatpaks.sh
+++
@@ -1,2 +0,0 @@
-ls /var/lib/flatpak/app/ | awk '{print "flatpak install --system " $1 " -y"}' > ./installed_flatpaks.sh
-ls ~/.local/share/flatpak/app | awk '{print "flatpak install --user " $1 " -y"}' > ./installed_user_flatpaks.sh
