aa07e216f193db553aa9ad5fd904c1b07ddf3b85
vikdevelop <vikdevelop@proton.me>
2024-08-30 18:44:48 +0200

    Update localization.py

--- src/localization.py
+++ src/localization.py
@@ -22,6 +22,7 @@ rel_notes = "<p>3.4-beta-07</p>\
 <li>This beta version is the last beta version before releasing the stable release version</li>\
 <li>Fixed a bug with auto-mounting the cloud drive folders when the folder mounts after the synchronization, so this is why the synchronization didn't work</li>\
 <li>In the synchronization mode, the Flatpak apps and their data will be installed immediately after the finished synchronization, so it is not necessary to log out of the system</li>\
+<li>New strings have been released for the application itself and also for the documentation related to the new release, so they can already be translated via Weblate</li>\
 </ul>\
 <p>3.4-beta-06-hotfix-1</p>\
 <p>Fixed a bug with creating the periodic saving file in the 'Set up the sync file' dialog and freezing the GUI during loading the mentioned dialog</p>\
