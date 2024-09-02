812b49f76f9675b860f7f1b5107a42dfcf6f4f2b
vikdevelop <vikdevelop@proton.me>
2024-09-02 15:39:37 +0200

    set release notes for the 3.4 stable version

--- src/localization.py
+++ src/localization.py
@@ -15,45 +15,15 @@ else:
     r_lang = p_lang[:-3]
 
 # Set application version, and icon
-v = "3.4-beta-07"
-icon = "io.github.vikdevelop.SaveDesktop.Devel"
-rel_notes = "<p>3.4-beta-07</p>\
-<ul>\
-<li>This beta version is the last beta version before releasing the stable release version</li>\
-<li>Fixed a bug with auto-mounting the cloud drive folders when the folder mounts after the synchronization, so this is why the synchronization didn't work</li>\
-<li>In the synchronization mode, the Flatpak apps and their data will be installed immediately after the finished synchronization, so it is not necessary to log out of the system</li>\
-<li>New strings have been released for the application itself and also for the documentation related to the new release, so they can already be translated via Weblate</li>\
-</ul>\
-<p>3.4-beta-06-hotfix-1</p>\
-<p>Fixed a bug with creating the periodic saving file in the 'Set up the sync file' dialog and freezing the GUI during loading the mentioned dialog</p>\
-<p>3.4-beta-06</p>\
-<ul>\
-<li>Removed support for synchronization using the URL in the local network, so it is only possible to use synchronization using the cloud drive folder, because it is untenable for me</li>\
-<li>Fixed bug with the canceling saving or importing configuation</li>\
-<li>Simplified setting up the bidirectional synchronization: just click on the 'Bidirectional synchronization' switch in the 'Connect to the cloud storage' dialog, and on the computer that makes periodic saving, select the same cloud drive folder in the already mentioned dialog.</li>\
-</ul>\
-<p>3.4-beta-05</p>\
-<p>Fixed bugs related to importing encrypted archives in the CLI interface, added more messages in the GUI in the error occur case, and excluded the cache directories of Flatpak apps in the configuration archive.</p>\
-<p>3.4-beta-04</p>\
-<ul>\
-<li>Fixed some bugs with setting up synchronization such as periodic synchronization interval selection or starting the HTTP server in the case if the selected synchronization in the local network only</li>\
-<li>Sped up the configuration import, because files that are unchanged will no longer be copied (this also applies to Flatpak app installations)</li>\
-<li>If an error occurs during saving or importing configuration, e.g., while copying the archive to the user-defined folder, it shows a message in the graphical user interface</li>\
-<li>In the cloud folder detection as the periodic saving folder case, the application will let you know by the icon next to the periodic saving file row in the 'Set up the sync file' dialog</li>\
-</ul>\
-<p>3.4-beta-03</p>\
-<ul>\
-<li>Simplified setting up the synchronization with the cloud folder process: on the Sync page, just click on the \"Set up the sync file\" button and make the changes required by the application</li>\
-<li>Added possibility to set up smaller window size</li>\
-</ul>\
-<p>3.4-beta-02</p>\
-<ul><li>Changed descriptions in the \"Connect with other computer\" dialog</li></ul>\
-<p>3.4-beta</p>\
-<ul>\
-<li>Added support for cloud synchronization: however, you need to have a folder on your computer that will be synchronized with your cloud storage, for example via GNOME Online Accounts.</li>\
-<li>Added an option to generate a password for the configuration archive, about 24 characters long</li>\
+v = "3.4"
+icon = "io.github.vikdevelop.SaveDesktop"
+rel_notes = "<ul>\
+<li>Synchronisation has been redesigned from the ground up - instead of using URLs to synchronise between computers on the local network, a folder synchronised with cloud storage (using GNOME Online Accounts or Rclone) is used - find out more here: https://vikdevelop.github.io/SaveDesktop/wiki/synchronization/en</li>\
+<li>Added support for comparing installed Flatpak apps, which means that only apps that have not yet been installed on the system will be installed</li>\
+<li>Added option to generate password for configuration archive</li>\
+<li>Improved support for displaying possible error messages in the GUI</li>\
+<li>Other minor UI improvements</li>\
 </ul>"
-
 flatpak = os.path.exists("/.flatpak-info")
 snap = os.environ.get('SNAP_NAME', '') == 'savedesktop'
 
