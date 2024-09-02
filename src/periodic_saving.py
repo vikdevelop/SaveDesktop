149379e412971a3fc44792b15978eb0fbd80432e
vikdevelop <vikdevelop@proton.me>
2024-09-02 17:01:10 +0200

    code cleanup

--- src/periodic_saving.py
+++ src/periodic_saving.py
@@ -1,28 +1,29 @@
-import argparse
+import argparse, json, os, gi
 from datetime import datetime
 from datetime import date
 from pathlib import Path
-import json
-import os
-import gi
 from gi.repository import GLib, Gio
 from localization import _, CACHE, DATA, home, system_dir, settings
 
-# get current datetime
+# Get the current date
 dt = datetime.now()
-# get day
+
+# Get the current day
 current_day = datetime.today()
-# get first day of month
+
+# Get the first day of month
 first_day = current_day.replace(day=1)
 
+# Get the user downloads directory
 download_dir = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOWNLOAD)
 
 class PeriodicBackups:
     def __init__(self):
         pass
-        
+
+    # Check, if the entered command has a --now argument or not
     def run(self, now: bool) -> None:
-        # Get directory for storing periodic backups
+        # If the periodic saving value returns the default value, set the correct directory for the periodic saving file
         if settings["periodic-saving-folder"] == '':
             self.pbfolder = f'{download_dir}/SaveDesktop/archives'
         else:
@@ -42,10 +43,11 @@ class PeriodicBackups:
         else:
             self.get_interval()
 
+    # Get selected periodic saving interval
     def get_interval(self):
         # Get periodic saving interval selected by the user
         if settings["periodic-saving"] == 'Never':
-            print("Periodic saving are not set up.")
+            print("Periodic saving is not set up.")
             exit()
         elif settings["periodic-saving"] == 'Daily':
             self.daily()
@@ -74,6 +76,7 @@ class PeriodicBackups:
 
     # Create backup
     def backup(self):
+        # Check if the default folder for periodic saving exists or not
         if self.pbfolder == f'{download_dir}/SaveDesktop/archives':
             try:
                 if not os.path.exists(f"{download_dir}/SaveDesktop/archives"):
@@ -82,18 +85,28 @@ class PeriodicBackups:
                 os.system(f"mkdir {home}/Downloads")
                 os.system(f"xdg-user-dirs-update --set DOWNLOAD {home}/Downloads")
                 os.makedirs(f"{download_dir}/SaveDesktop/archives")
+
+        # Check if the path {CACHE}/periodic_saving exists or not
         if not os.path.exists(f"{CACHE}/periodic_saving"):
             os.mkdir(f"{CACHE}/periodic_saving")
+
+        # Check if the filename format has spaces or not
         if " " in settings["filename-format"]:
             old_filename = f'{settings["filename-format"]}'
             filename = old_filename.replace(" ", "_")
         else:
             filename = settings["filename-format"]
+
+        # Write path to the periodic saving file to the file below
         with open(f"{CACHE}/.periodicfile.json", "w") as p:
             p.write('{\n "recent_file": "%s/%s.sd.tar.gz"\n}' % (self.pbfolder, filename))
+        
+        # Start saving the configuration
         os.chdir(f"{CACHE}/periodic_saving")
         from config import Save
         Save()
+
+        # Remove the file below after done saving configuration and start the self.config_saved() function
         os.system(f"rm {CACHE}/.periodicfile.json")
         self.config_saved()
 
