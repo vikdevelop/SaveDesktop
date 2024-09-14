7af36f4dc41fbad9966627e47b45dfee835a9724
vikdevelop <vikdevelop@proton.me>
2024-09-14 13:36:29 +0200

    fix a bug with installing Flatpak apps from Bash scripts

--- src/install_flatpak_from_script.py
+++ src/install_flatpak_from_script.py
@@ -39,29 +39,25 @@ elif os.path.exists(f"{CACHE_FLATPAK}/syncing/app"):
 installed_flatpaks_files = [f'{DATA_FLATPAK}/installed_flatpaks.sh', f'{DATA_FLATPAK}/installed_user_flatpaks.sh']
 system_flatpak_dir = '/var/lib/flatpak/app'
 user_flatpak_dir = os.path.expanduser('~/.local/share/flatpak/app')
-
-# load Flatpaks from bash scripts
+# Load Flatpaks from bash scripts
 installed_flatpaks = set()
 for file in installed_flatpaks_files:
     if os.path.exists(file):
         with open(file, 'r') as f:
-            for line in f:
-                if line.startswith('flatpak install'):
-                    installed_flatpaks.add(line.split()[3])
+            installed_flatpaks.update(line.split()[3] for line in f if line.startswith('flatpak install'))
 
-# get installed Flatpaks in the system and the home directories
+# Get installed Flatpaks in the specified directories
 installed_apps = set()
 for directory in [system_flatpak_dir, user_flatpak_dir]:
-    for app in os.listdir(directory):
-        if os.path.isdir(os.path.join(directory, app)):
-            installed_apps.add(app)
+    if os.path.exists(directory):
+        installed_apps.update(app for app in os.listdir(directory) if os.path.isdir(os.path.join(directory, app)))
 
-# compare Flatpaks listed in the Bash scripts with the Flatpaks installed in the system
+# Compare Flatpaks listed in the Bash scripts with the installed ones
 flatpaks_to_install = installed_flatpaks - installed_apps
 
 if flatpaks_to_install:
     for app in flatpaks_to_install:
         subprocess.run(['flatpak', 'install', '--user', app, '-y'])
-        os.system(f"rm -rf {DATA_FLATPAK}/*.sh")
+    os.system(f"rm -rf {DATA_FLATPAK}/*.sh")
 else:
     print('All Flatpak apps are installed.')
