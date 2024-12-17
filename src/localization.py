e456583930ed759dfe2b7396d827e5f899b803c4
vikdevelop <vikdevelop@proton.me>
2024-12-17 09:52:23 +0100

    Update localization.py

--- src/localization.py
+++ src/localization.py
@@ -15,9 +15,10 @@ else:
     r_lang = p_lang[:-3]
 
 # Set application version, and icon
-v = "3.5"
+v = "3.5.1-beta_2024.12.17"
 icon = "io.github.vikdevelop.SaveDesktop"
-rel_notes = "<ul>\
+rel_notes = "<p>Added support for saving the ~/.local/share/gvfs-metadata folder if the Desktop option is enabled, and fixed a bug with setting up the Nextcloud folder</p>\
+<p>3.5</p><ul>\
 <li>Added dialog for the initial setting up synchronization; if you have set up synchronization before, it doesn't show</li>\
 <li>Temporarily disabled support for synchronization in the Snap environment due to unreliability</li>\
 <li>Added more keyboard shortcuts</li>\
