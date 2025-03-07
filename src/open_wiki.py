4e5bd6ca1002ec4c11674d555f008e71a64b7cf5
vikdevelop <vikdevelop@proton.me>
2025-03-07 20:09:51 +0100

    Update open_wiki.py

--- src/open_wiki.py
+++ src/open_wiki.py
@@ -3,19 +3,16 @@ import locale, json, os, subprocess
 from localization import *
 
 if "The requested URL returned error: 404" in subprocess.getoutput(f"curl --head --fail https://raw.githubusercontent.com/vikdevelop/SaveDesktop/main/translations/wiki/{r_lang}.xml"):
-    pb_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/periodic-saving/en"
     flatpak_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/flatpak-apps-installation/en"
     sync_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/synchronization/en"
     enc_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/archive-encryption/en"
     lang_list = True
 elif r_lang == "en":
-    pb_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/periodic-saving/en"
     flatpak_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/flatpak-apps-installation/en"
     sync_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/synchronization/en"
     enc_wiki = "https://vikdevelop.github.io/SaveDesktop/wiki/archive-encryption/en"
     lang_list = False
 else:
-    pb_wiki = f"https://vikdevelop.github.io/SaveDesktop/wiki/periodic-saving/{r_lang}"
     flatpak_wiki = f"https://vikdevelop.github.io/SaveDesktop/wiki/flatpak-apps-installation/{r_lang}"
     sync_wiki = f"https://vikdevelop.github.io/SaveDesktop/wiki/synchronization/{r_lang}"
     enc_wiki = f"https://vikdevelop.github.io/SaveDesktop/wiki/archive-encryption/{r_lang}"
